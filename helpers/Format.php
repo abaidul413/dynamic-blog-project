95fb82aabb4aaaaa97f25d41f204558acc3d529b
Abaidul <cse.abaidul.sub@gmail.com>
2016-12-03 14:46:11 +0600

    show post from table

---
+++ helpers/Format.php
@@ -0,0 +1,19 @@
+<?php
+
+	class Format
+	{
+		public function formatDate($date)
+		{
+			return date('F j, Y, g:i a',strtotime($date));
+		}
+
+		public function shortText($text, $limit = 300)
+		{
+            $text = $text.' ';
+            $text = substr($text, 0,$limit);
+            $text = substr($text, 0, strrpos($text, ' '));
+            $text =$text ."...";
+            return $text;
+		}
+	}
+?>
--- index.php
+++ index.php
@@ -1,41 +1,49 @@
 <?php include'inc/header.php'; ?>
 <?php include'inc/slider.php'; ?>
+
+<?php include 'config/config.php'; ?>
+<?php include 'lib/Database.php'; ?>
+<?php include 'helpers/Format.php'; ?>
+
+  <?php
+
+     $db = new Database();
+     $fm = new Format();
+
+  ?>
+
+
 	
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
+
+           <?php 
+               $query = "select * from tbl_post limit 3";
+               $post = $db->select($query);
+
+               if($post){
+
+               	while ($result = $post->fetch_assoc()) {
+           ?>
+
 			<div class="samepost clear">
-				<h2><a href="">Our post title here</a></h2>
-				<h4>April 10, 2016, 12:30 PM, By <a href="#">Abir</a></h4>
-				 <a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-				<p>
-					Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.
-				</p>
+				<h2><a href="post.php?id=<?php echo $resutl['id']; ?>"><?php echo $result['title']; ?></a>
+				</h2>
+
+				<h4>
+				<?php echo $fm->formatDate($result['date']); ?>, By <a href="#"><?php echo $result['author']; ?></a>
+				</h4>
+
+				<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+
+				<?php echo $fm->shortText($result['body']); ?>
+
 				<div class="readmore clear">
-					<a href="post.php">Read More</a>
+					<a href="post.php?id=<?php echo $resutl['id']; ?>">Read More</a>
 				</div>
 			</div>
-			<div class="samepost clear">
-				<h2><a href="">Our post title here</a></h2>
-				<h4>April 10, 2016, 12:30 PM, By <a href="#">Abir</a></h4>
-				 <img src="images/post2.png" alt="post image"/>
-				<p>
-					Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.
-				</p>
-				<div class="readmore clear">
-					<a href="post.php">Read More</a>
-				</div>
-			</div>
-			<div class="samepost clear">
-				<h2>Our post title here</h2>
-				<h4>April 10, 2016, 12:30 PM, By <a href="#">Abir</a></h4>
-				 <img src="images/post1.jpg" alt="post image"/>
-				<p>
-					Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.
-				</p>
-				<div class="readmore clear">
-					<a href="post.php">Read More</a>
-				</div>
-			</div>
+    <?php } ?>	<!-- End of whilop -->	
+    <?php  }else{header("location:404.php");}  ?>
 
 		</div>
 		
