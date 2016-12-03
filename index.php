8a2b57d01614650dc2df9d1b8a0602abebf1e1c5
Abaidul <cse.abaidul.sub@gmail.com>
2016-12-03 17:32:31 +0600

    Pagination added

--- index.php
+++ index.php
@@ -16,9 +16,24 @@
 	
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
+           
+           <!-- Pagination process -->
+           <?php
+               $per_page = 3;
+               if(isset($_GET['page']))
+               {
+               	  $page = $_GET['page'];
+               }else{
+               	   $page = 1;
+               }
+               $start_form = ($page-1)*$per_page;
+
+           ?>
+           <!-- Pagination process End-->
+
 
            <?php 
-               $query = "select * from tbl_post limit 3";
+               $query = "select * from tbl_post limit $start_form,$per_page";
                $post = $db->select($query);
 
                if($post){
@@ -34,7 +49,7 @@
 				<?php echo $fm->formatDate($result['date']); ?>, By <a href="#"><?php echo $result['author']; ?></a>
 				</h4>
 
-				<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+				<a href="#"><img src="admin/upload/<?php echo $result['image']; ?>" alt="post image"/></a>
 
 				<?php echo $fm->shortText($result['body']); ?>
 
@@ -43,7 +58,26 @@
 				</div>
 			</div>
     <?php } ?>	<!-- End of whilop -->	
-    <?php  }else{header("location:404.php");}  ?>
+
+    <!-- Pagination -->
+
+      <?php 
+       
+	       $query = "select * from tbl_post";
+	       $result = $db->select($query);
+	       $total_row = mysqli_num_rows($result);
+	       $total_pages = ceil($total_row/$per_page);
+
+	       echo "<span class='pagination'><a href='index.php?page=1'>".'First page'."</a>";
+	       for ($i=1; $i <=$total_pages ; $i++) { 
+	        	echo "<a href = 'index.php?page=".$i."'>".$i."</a>";
+	        } 
+	       echo "<a href='index.php?page= $total_pages'>".'Last page'."</a></span>"
+       ?>
+
+    <!-- End of pagination -->
+
+     <?php  }else{header("location:404.php");}  ?>
 
 		</div>
 		
