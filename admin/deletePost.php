5ef04cbe888c54ab866d441569c656628271fbc4
Abaidul <cse.abaidul.sub@gmail.com>
2016-12-06 16:42:05 +0600

    Delete igm from project folder by admin panel

--- admin/deletePost.php
+++ admin/deletePost.php
@@ -13,11 +13,20 @@
     <div class="box round first grid">
         <h2>Delete Post</h2>
         <div class="block">
-          <?php 
+          <?php
+              $img = "select * from tbl_post where id = '$id'";
+              $img_delete = $db->select($img);
+              if($img_delete){
+                  while ($delete_result = $img_delete->fetch_assoc()) {
+                      $link_img = $delete_result['image'];
+                      unlink($link_img);
+                  }
+              } 
               $query = "DELETE FROM tbl_post WHERE id ='$id' ";
               $is_delete = $db->delete($query);
               if ($is_delete) {
                  echo "<span class='sucess'>Data Deleted Successfully!</span></span>";
+                 //header("Location:postlist.php");
               }else{
                 echo "<span class='error'>Failed To Delete Post !</span>";
               }
