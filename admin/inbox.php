0a0fbc87202f2ffc8b464870eccb1e39320dc0b2
Abaidul <cse.abaidul.sub@gmail.com>
2016-12-09 05:23:44 +0600

    seen msg move to seen box

--- admin/inbox.php
+++ admin/inbox.php
@@ -1,9 +1,25 @@
 ﻿<?php include"inc/header.php" ?>
 <?php include"inc/sidebar.php" ?>
 
+
 <div class="grid_10">
     <div class="box round first grid">
         <h2>Inbox</h2>
+
+        <?php 
+		  if (isset($_GET['seenId'])) {
+		  	$seenId = $_GET['seenId'];
+
+		  	$query = "UPDATE tbl_contact SET status = '1' WHERE id = '$seenId'";
+		  	$update = $db->update($query);
+		  	if ($update) {
+		  		echo "<span style = 'color:green; font-weight:bold;'>Message Send To Seen Box</span>";
+		  	}else{
+		  		echo "<span style = 'color:green; font-weight:bold;'>Faild to send messege in seen box</span>";
+		  	}
+		  }
+      ?>
+
         <div class="block">        
             <table class="data display datatable" id="example">
 			<thead>
@@ -36,7 +52,7 @@
 					  <a href="viewMessage.php?msgId=<?php echo $result['id']; ?>">View</a> || 
 					  <a href="replyMessage.php?rplyId=<?php echo $result['id']; ?>">Reply
 					  </a>||
-					   <a href="?seenId=<?php echo $result['id']; ?>">Seen</a>
+					   <a onclick="return confirm('Are Sure To Move The Message?');" href="?seenId=<?php echo $result['id']; ?>">Seen</a>
 					</td>
 				</tr>
        <?php } } ?>
@@ -60,16 +76,23 @@
 				</tr>
 			</thead>
 			<tbody>
-
+         <?php 
+             $query = "select * from tbl_contact where status = '1'";
+             $select = $db->select($query);
+             if ($select) {
+             	$i = 0;
+             	while ($result = $select->fetch_assoc()) {
+                   $i++;
+          ?>
 				<tr class="odd gradeX">
-					<td>01</td>
-					<td>Internet</td>
-					<td>Internet</td>
-					<td>Internet</td>
-					<td>Internet</td>
-					<td> <a href="">Delete</a> </td>
+					<td><?php echo $i; ?></td>
+					<td><?php echo $result['firstname'].' '.$result['lastname']; ?></td>
+					<td><?php echo $result['email']; ?></td>
+					<td><?php echo $fm->formatDate($result['msg']); ?></td>
+					<td><?php echo $fm->formatDate($result['date']); ?></td>
+					<td> <a onclick="return confirm('Are Sure To Move The Message?');" href="?delId = <?php echo $result['id'];?>">Delete</a> </td>
 				</tr>
-
+       <?php } } ?>
 			</tbody>
 		</table>
        </div>
